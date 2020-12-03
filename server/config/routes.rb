1f1caa157ef8870c32cc61cc931a6a8be11c3110
Hafidz Mahrus <havus.it@gmail.com>
2020-12-03 21:21:53 +0800

    add transaction route

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, format: :json do
     namespace :v1 do
       resources :products, only: [:index, :show, :create, :update]
+      resources :sales_transactions, only: [:index, :show, :create, :update]
     end
   end
 end

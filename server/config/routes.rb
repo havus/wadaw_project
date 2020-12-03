4a8bb1e37e646c51a2366a4919cd1a49f3d510a6
Hafidz Mahrus <havus.it@gmail.com>
2020-12-03 21:20:23 +0800

    refactor routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, format: :json do
     namespace :v1 do
-      resources :products, only: [:index, :show, :create, :update, :destroy]
+      resources :products, only: [:index, :show, :create, :update]
     end
   end
 end

900b55917c6e0f4300131e7bc7005d4d24942856
Hafidz Mahrus <havus.it@gmail.com>
2022-01-30 16:21:00 +0800

    refactor spacing and navbar links on click

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -1,24 +1,26 @@
 <!-- Please remove this file from your project -->
 <template>
-  <header class="fixed z-50 w-full flex items-center justify-between flex-wrap bg-gray-blue-50 py-4 px-8 lg:px-32">
-    <img :src="wadawLogo" class="max-h-10 mr-3 lg:-ml-4" alt="Wadaw logo">
+  <header class="bg-gray-blue-50 fixed z-50 w-full">
+    <div class="container px-8 lg:px-32 mx-auto flex items-center justify-between flex-wrap py-2">
+      <img :src="wadawLogo" class="max-h-9 mr-3 -ml-3" alt="Wadaw logo">
 
-    <div class="block md:hidden">
-      <button class="flex items-center px-3 py-2 border rounded border-gray-400 text-gray-400" @click="toggle">
-        <FontAwesomeIcon :icon="['fas', 'bars']" />
-      </button>
-    </div>
-    <div :class="open ? 'block': 'hidden'" class="w-full flex-grow md:flex md:items-center md:w-auto">
-      <div class="text-sm md:flex-grow">
-        <NuxtLink class="font-semibold text-gray-600 no-underline block p-2 pl-0 md:inline-block" to="/">
-          Home
-        </NuxtLink>
-        <NuxtLink class="font-semibold text-gray-600 no-underline block p-2 pl-0 md:inline-block" to="/projects">
-          Projects
-        </NuxtLink>
-        <NuxtLink class="font-semibold text-gray-600 no-underline block p-2 pl-0 md:inline-block" to="/about">
-          About
-        </NuxtLink>
+      <div class="block md:hidden">
+        <button class="flex items-center px-3 py-2 border rounded border-gray-400 text-gray-400" @click="toggle">
+          <FontAwesomeIcon :icon="['fas', 'bars']" />
+        </button>
+      </div>
+
+      <div :class="open ? 'block': 'hidden'" class="w-full flex-grow md:flex md:items-center md:w-auto">
+        <div class="text-sm md:flex-grow">
+          <NuxtLink
+            v-for="(link, i) in links" :key="i"
+            :to="link.path"
+            class="font-semibold text-gray-600 no-underline block p-2 pl-0 md:inline-block"
+            @click.native="toggle"
+          >
+            {{ link.text }}
+          </NuxtLink>
+        </div>
       </div>
     </div>
   </header>
@@ -33,6 +35,11 @@ export default {
     return {
       wadawLogo,
       open: false,
+      links: [
+        { path: '/', text: 'Home' },
+        { path: '/projects', text: 'Projects' },
+        { path: '/about', text: 'About' },
+      ],
     };
   },
   methods: {
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,47 +1,53 @@
 <template>
   <div class="text-gray-600">
-    <div class="bg-gray-blue-50 min-h-96 p-12 lg:p-32 flex flex-wrap flex-col items-center lg:flex-row justify-center lg:justify-between">
-      <div class="max-w-112 h-full mt-12 lg:my-auto order-last lg:order-first">
-        <h1 class="text-4xl mb-5 font-semibold bg-test-50">
-          Wadaw Project
-        </h1>
-        <p class="font-light">
-          Embracing people in all fields by providing a platform for them to make their ideas happen.
-          Help realize ideas without take their copyright.
-        </p>
-      </div>
+    <div class="bg-gray-blue-50 py-12">
+      <div class="container p-8 lg:px-32 mx-auto min-h-96 flex flex-wrap flex-col items-center lg:flex-row justify-center lg:justify-between">
+        <div class="max-w-112 h-full mt-12 lg:my-auto lg:pr-7 order-last lg:order-first">
+          <h1 class="text-4xl mb-5 font-semibold bg-test-50">
+            Wadaw Project
+          </h1>
+          <p class="font-light">
+            Embracing people in all fields by providing a platform for them to make their ideas happen.
+            Help realize ideas without take their copyright.
+          </p>
+        </div>
 
-      <img :src="wadawChipset" class="mt-10 lg:mt-0 max-h-60 lg:max-h-80 xl:mr-32" alt="Wadaw chipset">
+        <img :src="wadawChipset" class="mt-10 lg:mt-0 max-h-60 lg:max-h-80" alt="Wadaw chipset">
+      </div>
     </div>
 
     <!-- Gain More Experience with Us -->
-    <div class="bg-white container mx-auto px-5">
-      <h2 class="text-3xl text-center font-semibold bg-test-50 my-12">
-        Gain More Experience with Us
-      </h2>
+    <div class="bg-white">
+      <div class="container p-8 mx-auto">
+        <h2 class="text-3xl text-center font-semibold bg-test-50 my-12">
+          Gain More Experience with Us
+        </h2>
 
-      <div class="lg:px-28 flex flex-wrap justify-center lg:justify-start">
-        <div
-          v-for="(experience, i) in experiences" :key="i"
-          class="lg:w-1/2 xl:w-1/3 lg:mx-auto my-5 lg:my-3"
-        >
-          <div class="w-full">
-            <div class="p-5">
-              <div class="flex flex-col items-center lg:items-start">
-                <div class="w-16 h-16 border rounded-full border-yellow-500 flex justify-center items-center">
-                  <div class="text-3xl text-yellow-500">
-                    <FontAwesomeIcon :icon="experience.icon" />
+        <div class="lg:px-28 flex flex-wrap justify-center lg:justify-start">
+          <div
+            v-for="(experience, i) in experiences" :key="i"
+            class="lg:w-1/2 xl:w-1/3 lg:mx-auto my-5 lg:my-3"
+          >
+            <div class="w-full">
+              <div class="p-5">
+                <div class="flex flex-col items-center">
+                  <!-- bg-white rounded-full filter drop-shadow-lg border-yellow-500 flex flex-col justify-between -->
+                  <!-- <div class="w-16 h-16 border rounded-full border-yellow-500 flex justify-center items-center"> -->
+                  <div class="w-16 h-16 bg-white rounded-full filter drop-shadow-md flex justify-center items-center">
+                    <div class="text-3xl text-yellow-500">
+                      <FontAwesomeIcon :icon="experience.icon" />
+                    </div>
                   </div>
+
+                  <h3 class="text-2xl mt-3 mb-2">
+                    {{ experience.title }}
+                  </h3>
                 </div>
 
-                <h3 class="text-2xl mt-3 mb-2">
-                  {{ experience.title }}
-                </h3>
+                <p class="font-light text-center">
+                  {{ experience.text }}
+                </p>
               </div>
-
-              <p class="font-light text-center lg:text-left">
-                {{ experience.text }}
-              </p>
             </div>
           </div>
         </div>
@@ -52,8 +58,8 @@
       <div class="w-112 h-0.5 bg-gradient-to-r from-white via-gray-200 to-white my-20" />
     </div>
 
-    <div class="bg-gray-blue-50 bg-gradient-to-b from-white pt-5">
-      <div class="container mx-auto lg:px-28 flex flex-wrap">
+    <div class="bg-gray-blue-50 bg-gradient-to-b from-white">
+      <div class="container p-8 lg:px-32 mx-auto flex flex-wrap">
         <div
           v-for="(quote, i) in quotes" :key="i"
           class="p-14 pt-0 lg:p-12 xl:p-5 w-full lg:w-1/2 xl:w-1/3 mx-auto"
@@ -79,34 +85,36 @@
       </div>
     </div>
 
-    <div class="bg-gray-blue-50 min-h-96 p-8 pb-56 md:p-16 md:pb-56 lg:p-32 lg:pb-56 flex flex-col items-start">
-      <div class="h-16 w-16 rounded-full flex items-center justify-center border border-gray-400">
-        <div class="bg-blue-300 rounded-full h-14 w-14 text-4xl text-white flex items-center justify-center">
-          <FontAwesomeIcon :icon="['fab', 'react']" />
+    <div class="bg-gray-blue-50 min-h-96">
+      <div class="container p-8 lg:px-32 mx-auto pb-56 md:pb-56 lg:p-32 lg:pb-56 flex flex-col items-start">
+        <div class="h-16 w-16 rounded-full flex items-center justify-center border border-gray-400">
+          <div class="bg-blue-300 rounded-full h-14 w-14 text-4xl text-white flex items-center justify-center">
+            <FontAwesomeIcon :icon="['fab', 'react']" />
+          </div>
         </div>
+
+        <span class="font-semibold text-blue-400 mt-6">Build anything</span>
+        <h3 class="text-4xl font-semibold mt-2">
+          Build whatever from your idea, seriously.
+        </h3>
+        <p class="max-w-3xl mt-4 text-gray-500">
+          Turning an Idea to a business.
+          Figure out what problem is being solved.
+          Find your market.
+          Find your support, have you ever noticed how few successful startups were founded by just one person?
+          So, here we are. To build the MVP.
+        </p>
+
+        <button
+          class="py-2 px-5 rounded-full flex items-center mt-5 text-sm font-semibold
+          text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
+        >
+          <span class="inline">Contact Us</span>
+          <div class="ml-2">
+            <FontAwesomeIcon :icon="['fas', 'chevron-right']" />
+          </div>
+        </button>
       </div>
-
-      <span class="font-semibold text-blue-400 mt-6">Build anything</span>
-      <h3 class="text-4xl font-semibold mt-2">
-        Build whatever from your idea, seriously.
-      </h3>
-      <p class="max-w-3xl mt-4 text-gray-500">
-        Turning an Idea to a business.
-        Figure out what problem is being solved.
-        Find your market.
-        Find your support, have you ever noticed how few successful startups were founded by just one person?
-        So, here we are. To build the MVP.
-      </p>
-
-      <button
-        class="py-2 px-5 rounded-full flex items-center mt-5 text-sm font-semibold
-        text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
-      >
-        <span class="inline">Contact Us</span>
-        <div class="ml-2">
-          <FontAwesomeIcon :icon="['fas', 'chevron-right']" />
-        </div>
-      </button>
     </div>
   </div>
 </template>
